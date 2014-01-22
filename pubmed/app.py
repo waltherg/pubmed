c2006e73f2043a8dfb137917ef75878f86a2707b
Georg Walther <georgwalther@gmail.com>
2014-01-22 10:26:51 +0000

    receive PMID from form and redirect browser to summary page

--- pubmed/app.py
+++ pubmed/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from summarize import summarize
 
 app = Flask(__name__)
@@ -10,6 +10,15 @@ def index():
 @app.route('/<int:pmid>')
 def get_summary(pmid=None):
     return summarize(None, pmid)
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    error = None
+    if request.method == 'POST':
+        pmid = request.form['pmid']
+        return redirect('/'+pmid)
+    else:
+        return 'Method not available'
 
 if __name__ == '__main__':
     app.run(debug=True)
