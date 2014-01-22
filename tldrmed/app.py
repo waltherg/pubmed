2efe0ba0e9f195557250a254f4f28647b3874029
Georg Walther <georgwalther@gmail.com>
2014-01-22 00:40:38 +0000

    add summarizer URL and summarizer endpoint

--- tldrmed/app.py
+++ tldrmed/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from summarize import summarize
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('tldrmed.html')
+
+@app.route('/<int:pmid>')
+def get_summary(pmid=None):
+    return summarize(None, pmid)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tldrmed/summarize.py
@@ -0,0 +1,3 @@
+def summarize(body, pmid):
+    if not body:
+        return('No summary avialable for PMID %d' % pmid)
