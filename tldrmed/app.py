45fd48161395c5fb5e5ee84e6bdef64bc7d1d626
Georg Walther <georgwalther@gmail.com>
2014-01-22 00:29:12 +0000

    render template

--- tldrmed/app.py
+++ tldrmed/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'TL;DR med'
+    return render_template('layout.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
