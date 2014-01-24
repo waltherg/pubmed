784698045d4e733676422156a7b314efa60aaa70
Georg Walther <georgwalther@gmail.com>
2014-01-24 10:18:38 +0000

    pickle and time summary creation

--- pubmed/app.py
+++ pubmed/app.py
@@ -4,6 +4,9 @@ import requests
 from lxml import etree
 from bs4 import BeautifulSoup as BS
 
+import cPickle as pickle
+from time import time
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +15,26 @@ def index():
 
 @app.route('/<int:pmid>')
 def get_summary(pmid=None):
+    try:
+        summary_time = time()
+        summary, url = pickle.load(open('%d.summary' % pmid, 'r'))
+        summary_time = time() - summary_time
+        print('Retrieve pickled summary for PMID %d: %.4f s' % (pmid,
+                                                                summary_time))
+    except:
+        summary_time = time()
+        summary, url = make_summary(pmid)
+        pickle.dump([summary, url], open('%d.summary' % pmid, 'w'))
+        summary_time = time() - summary_time
+        print('Remake & pickle summary for PMID %d: %.4f s' % (pmid,
+                                                               summary_time))
+
+    return render_template('tldrmed.html',
+                           article_summary=summary,
+                           article_url=url,
+                           request_pmid=pmid)
+
+def make_summary(pmid=None):
     r = requests.get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
                        'elink.fcgi?dbfrom=pubmed&id=%d&cmd=prlinks'
                        '&retmode=json' % pmid)
@@ -21,16 +44,16 @@ def get_summary(pmid=None):
         xml = etree.fromstring(r.text)
         try:
             url = xml.xpath('//Url')[0].text
+            full_text_r = requests.get(url)
+            article = BS(full_text_r.text)
+            paragraphs = article.findAll(['p'])
+            body = ' '.join([p.text for p in paragraphs])
+            summary = summarize(body, pmid)
         except:
-            return('PubMed provided no fulltext URL for PMID %d' %pmid)
-        full_text_r = requests.get(url)
-        article = BS(full_text_r.text)
-        paragraphs = article.findAll(['p'])
-        body = ' '.join([p.text for p in paragraphs])
+            summary = ['PubMed provided no full text URL for PMID %d' %pmid]
+            url = None
 
-    summary = summarize(body, pmid)
-    
-    return render_template('tldrmed.html', article_summary=summary)
+    return summary, url
 
 @app.route('/search', methods=['POST', 'GET'])
 def search():
