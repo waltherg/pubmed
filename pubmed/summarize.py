b45e29b4eb76863c12cf573200acc6fae1c481bc
Georg Walther <georgwalther@gmail.com>
2014-01-24 10:19:16 +0000

    pickle and time tagger training

--- pubmed/summarize.py
+++ pubmed/summarize.py
@@ -3,9 +3,35 @@ from nltk.corpus import brown
 import nltk
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 
+import cPickle as pickle
+
+from time import time
+
 brown_train = brown.tagged_sents()
-unigram_tagger = UnigramTagger(brown_train)
-bigram_tagger = BigramTagger(brown_train, backoff=unigram_tagger)
+
+try:
+    unigram_time = time()
+    unigram_tagger = pickle.load(open('unigram.object', 'r'))
+    unigram_time = time() - unigram_time
+    print('Loaded pickled unigram tagger: %.4f s' % unigram_time)
+except:
+    unigram_time = time()
+    unigram_tagger = UnigramTagger(brown_train)
+    pickle.dump(unigram_tagger, open('unigram.object', 'w'))
+    unigram_time = time() - unigram_time
+    print('Retrained and pickled unigram tagger: %.4f s' % unigram_time)
+
+try:
+    bigram_time = time()
+    bigram_tagger = pickle.load(open('bigram.object', 'r'))
+    bigram_time = time() - bigram_time
+    print('Loaded pickled bigram tagger: %.4f s' % bigram_time)
+except:
+    bigram_time = time()
+    bigram_tagger = BigramTagger(brown_train, backoff=unigram_tagger)
+    pickle.dump(bigram_tagger, open('bigram.object', 'w'))
+    bigram_time = time() - bigram_time
+    print('Retrained and pickled bigram tagger: %.4f s' % bigram_time)
 
 def summarize(body, pmid):
     if not body:
